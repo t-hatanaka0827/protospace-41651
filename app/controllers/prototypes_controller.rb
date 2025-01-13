b8f33a07d2130418574382ddff367801d5b6a454
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-01-13 22:03:51 +0900

    createアクションの編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,21 @@
 class PrototypesController < ApplicationController
   def index
+
   end
   def new
     @prototype = Prototype.new
   end
 
   def create
-
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
   end
 
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
 end
