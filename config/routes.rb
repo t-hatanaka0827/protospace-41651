24651837c8fb32f65dcdbe8a819fa06b67b583ab
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-01-15 00:57:52 +0900

    destroyアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "prototypes#index"
-  resources :prototypes, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :new, :create, :edit, :update, :destroy, :show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
