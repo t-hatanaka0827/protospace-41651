8f4ab0e445939c23dcb1859d2d7a91174762e09d
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-01-16 00:18:17 +0900

    prototypeコントローラー、showアクション

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "prototypes#index"
-  resources :prototypes, only: [:index, :new, :create, :edit, :update, :destroy, :show]
+  resources :prototypes, only: [:index, :new, :create, :edit, :update, :destroy, :show] do
+    resources :comments, only: [:create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
