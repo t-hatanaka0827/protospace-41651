03e71f618283dc13d4c4ba5d368fcffd602f6edc
t-hatanaka0827 <otsukimi0827@gmail.com>
2025-01-15 22:41:24 +0900

    commentモデル、コントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
          validates :profile, presence: true
          validates :occupation, presence: true
          validates :position, presence: true
-         has_many :prototypes
+         has_many :prototypes, dependent: :destroy        
 end
